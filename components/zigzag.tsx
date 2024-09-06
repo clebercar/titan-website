4a9ae34ea2f41f6b4b84450b3a57d82626d8fc22
Cleber Carvalho <contatoclebercarvalho@gmail.com>
2024-09-06 10:52:01 -0300

    fix: adjust Portuguese orthographic

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -11,7 +11,7 @@ export default function Zigzag() {
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">
-              Atinga os resultados que importam
+              Atinja os resultados que importam
             </div>
             <h1 className="h2 mb-4">Movimente a sua carreira</h1>
             <p className="text-xl text-gray-400">
